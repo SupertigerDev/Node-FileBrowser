396972b72efb313c03c7c354ae05aa13628d7f2f
supertiger1234 <saawalkhn@googlemail.com>
2018-05-07 10:30:19 +0100

    Added transparent layout and some of the gui.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const electron = require('electron');
 const {app, BrowserWindow} = electron;
+var electronVibrancy = require('electron-vibrancy');
 const testFolder = 'C:/';
 const fs = require('fs');
 
 app.on('ready', ()=> {
-    let win = new BrowserWindow({width:800, height:600, frame: false});
+    let win = new BrowserWindow({
+        width:800,
+        height:600,
+        frame: false,
+        transparent: true
+    });
+    electronVibrancy.SetVibrancy(win, 0);
     win.loadURL(`file://${__dirname}/site/index.html`)
 });
 
